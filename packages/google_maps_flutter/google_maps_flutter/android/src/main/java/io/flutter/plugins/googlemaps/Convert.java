c3eb54d6027f81da90974f2ece55f49c8cd589d2
MuBirhan <birhan.muradasil@rodeapps.com>
2020-09-10 10:44:55 +0300

    bla

--- packages/google_maps_flutter/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/Convert.java
+++ packages/google_maps_flutter/google_maps_flutter/android/src/main/java/io/flutter/plugins/googlemaps/Convert.java
@@ -223,6 +223,7 @@ class Convert {
       return null;
     }
     final List<?> data = toList(o);
+
     return LatLngBounds.builder().include(toLatLng(data.get(0))).include(toLatLng(data.get(1))).build();
   }
 
